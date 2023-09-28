5d703195a59e7518c85dd2ec281708f3c1919789
anplaceb <anplaceb@gmail.com>
2023-09-28 12:48:21 +0200

    More flexible input

--- intersection_over_union.py
+++ intersection_over_union.py
@@ -1,13 +1,13 @@
+"""Given a reference geometry and a detection geometry, calculate intersection over union and write it to .txt file"""
 import arcpy
 import os
 import re
 
 # Paths
-reference_data_Friederike_2018 = r"D:\wsf-sat\data\validation\Friedericke_2018\shp\Friederike_2018_kartiergebiet_zeitangepasst_nachkartiert260421_filter025.shp"
-reference_data_Friederike_2019 = r"D:\wsf-sat\data\validation\Friedericke_2019\fried_2019_kartiergebiet_zeitangepasst_nachkartiert_260421_filter025.shp"
-reference_data_Harz_2021 = r"D:\wsf-sat\data\validation\Harz_2021\shp\Harz_Freiflaeche_stehendesTotholz_2021_025_clipNds.shp"
-detection_folder = r"D:\wsf-sat\methods\threshold_analysis\03_postprocessing"
-output_folder = r"D:\wsf-sat\methods\threshold_analysis\04_IntersectOverUnion"
+reference_data = r"D:\wsf-sat\data\validation\Harz_Freiflaeche_Totholz_2018_2021\Harz_Freifl_Totholz_2020_ni.shp"
+detection_folder = r"D:\wsf-sat\methods\validation\intersect_over_union_rf\input_detection\harz_2020"
+output_folder = r"D:\wsf-sat\methods\validation\intersect_over_union_rf\output\harz_2020"
+id_detection = "harz_2020"
 
 if not os.path.isdir(os.path.join(output_folder, "IntersectOverUnion.gdb")):
     arcpy.CreateFileGDB_management(output_folder, "IntersectOverUnion")
@@ -26,31 +26,12 @@ print(list(map(os.path.basename, detection_files)))
 
 
 # Create textfile to write results
-if not os.path.isfile(os.path.join(output_folder, "thresholds_results.txt")):
-    with open(os.path.join(output_folder, "thresholds_results.txt"), "w") as a:
-        a.write("threshold year area_intersect_ha area_union_ha intersection_over_union" + "\n")
+if not os.path.isfile(os.path.join(output_folder, "results.txt")):
+    with open(os.path.join(output_folder, "results.txt"), "w") as a:
+        a.write("id area_intersect_ha area_union_ha intersection_over_union" + "\n")
     a.close()
 
 for detection_file in detection_files:
-    year = re.findall(r"(?<!\d)\d{4}(?!\d)", os.path.basename(detection_file))[0]  # search for current year
-    print(f'file: {os.path.basename(detection_file)}, year: {year}')
-    if year == "2018":
-        reference_data = reference_data_Friederike_2018
-        print(f"Year: {year}, reference data: {reference_data}")
-    elif year == "2019":
-        reference_data = reference_data_Friederike_2019
-        print(f"Year: {year}, reference data: {reference_data}")
-    elif year == "2021":
-        reference_data = reference_data_Harz_2021
-        print(f"Year: {year}, reference data: {reference_data}")
-    else:
-        print("No reference data for this year")
-        break
-
-    threshold = re.findall(r"(?<!\d)m\d{2}(?!\d)", os.path.basename(detection_file))[0]
-
-    id_detection = f"{threshold}_{year}"
-    print(id_detection)
 
     # Intersect reference and detection
     arcpy.Intersect_analysis(in_features=[reference_data, detection_file],
@@ -80,9 +61,10 @@ for detection_file in detection_files:
             area_sum_union = row[0]
             print(area_sum_union)
 
-    with open(os.path.join(output_folder, "thresholds_results.txt"), "a") as a:
+    with open(os.path.join(output_folder, "results.txt"), "a") as a:
         intersection_over_union = round(area_sum_intersect/area_sum_union * 100, 2)
-        a.write(" ".join([threshold, year, str(area_sum_intersect/10000),
+        print(intersection_over_union)
+        a.write(" ".join([id_detection, str(area_sum_intersect/10000),
                           str(area_sum_union/10000), str(intersection_over_union)]) +"\n")
         a.close()
 
